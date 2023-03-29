5bdb3452cacdb4ac89a82f3c5957307dec088f3c
Bogdan Burlacu <bogdan.burlacu@pm.me>
2023-03-29 20:22:41 +0200

    update flake

--- flake.nix
+++ flake.nix
@@ -6,44 +6,36 @@
   inputs.nixpkgs.url = "github:nixos/nixpkgs";
 
   outputs = { self, flake-utils, nixpkgs, foolnotion }:
-    flake-utils.lib.eachDefaultSystem
-      (system:
-        let
-          pkgs = import nixpkgs {
-            inherit system;
-            overlays = [ foolnotion.overlay ];
-          };
-          buildTesting = pkgs.targetPlatform.isx86_64;
-        in rec
-        {
-          packages.default = pkgs.stdenv.mkDerivation {
-            name = "vstat";
-            src = self;
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ foolnotion.overlay ];
+        };
+      in rec {
+        packages.default = pkgs.stdenv.mkDerivation {
+          name = "vstat";
+          src = self;
 
-            cmakeFlags = [
-              "-DCMAKE_BUILD_TYPE=Release"
-              "-DBUILD_TESTING=${if buildTesting then "ON" else "OFF"}"
-            ];
+          cmakeFlags = [ "-DCMAKE_BUILD_TYPE=Release" ];
 
-            nativeBuildInputs = with pkgs; [ cmake ];
-            buildInputs = with pkgs; [
-                # python environment for bindings and scripting
-                boost
-                doctest
-                gsl
-                pkg-config
-                eve
-              ] ++ lib.optionals buildTesting [ linasm ];
-          };
+          nativeBuildInputs = with pkgs; [ cmake ];
+          buildInputs = with pkgs; [ eve ];
+        };
 
-          devShells.default = pkgs.stdenv.mkDerivation {
-            name = "vstat-dev";
-            hardeningDisable = [ "all" ];
-            impureUseNativeOptimizations = true;
-            nativeBuildInputs = with pkgs; [ cmake clang_14 clang-tools cppcheck gdb ];
-
-            buildInputs = packages.default.buildInputs;
-          };
-        }
-      );
+        devShells.default = pkgs.stdenv.mkDerivation {
+          name = "vstat-dev";
+          hardeningDisable = [ "all" ];
+          impureUseNativeOptimizations = true;
+          nativeBuildInputs = with pkgs; [
+            cmake
+            clang_15
+            clang-tools
+            cppcheck
+            gdb
+          ];
+          buildInputs = packages.default.buildInputs
+            ++ (with pkgs; [ boost doctest gsl linasm pkg-config ]);
+        };
+      });
 }
