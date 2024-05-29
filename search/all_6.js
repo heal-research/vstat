f4e4422bde0af097fb2cd1747879151ca81dd539
foolnotion <foolnotion@users.noreply.github.com>
2024-05-29 20:29:42 +0000

    deploy: 0ce401bb8da6d3ccfae987ddff42f2a17d9a0637

---
+++ search/all_6.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['r2_5fscore_0',['r2_score',['../group__Metrics.html#gab2d9413781c12cbbc109f27a29e64c94',1,'VSTAT_NAMESPACE::metrics::r2_score(I first1, std::sentinel_for&lt; I &gt; auto last1, J first2) noexcept -&gt; double'],['../group__Metrics.html#ga6e7d665d904a6723389aa59843349e84',1,'VSTAT_NAMESPACE::metrics::r2_score(I first1, std::sentinel_for&lt; I &gt; auto last1, J first2, K first3) noexcept -&gt; double']]],
+  ['regression_20metrics_1',['Regression metrics',['../group__Metrics.html',1,'']]]
+];
---
+++ search/all_7.js
@@ -0,0 +1,7 @@
+var searchData=
+[
+  ['simd_20primitives_0',['Vectorized statistics using SIMD primitives',['../index.html#autotoc_md0',1,'']]],
+  ['statistics_1',['Available statistics',['../index.html#autotoc_md4',1,'']]],
+  ['statistics_2',['statistics',['../group__Bivariate.html',1,'Bivariate statistics'],['../group__Univariate.html',1,'Univariate statistics']]],
+  ['statistics_20using_20simd_20primitives_3',['Vectorized statistics using SIMD primitives',['../index.html#autotoc_md0',1,'']]]
+];
