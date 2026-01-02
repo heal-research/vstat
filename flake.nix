ea5a8176ab605b2f55b804a0de77354336fa2d6c
Bogdan Burlacu <bogdan.burlacu@pm.me>
2026-01-02 11:14:41 +0200

    update flake

--- flake.nix
+++ flake.nix
@@ -8,26 +8,30 @@
     foolnotion.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = inputs@{ self, flake-parts, nixpkgs, foolnotion }:
+  outputs =
+    inputs@{
+      self,
+      flake-parts,
+      nixpkgs,
+      foolnotion,
+    }:
     flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
+      systems = [
+        "x86_64-linux"
+        "x86_64-darwin"
+        "aarch64-linux"
+        "aarch64-darwin"
+      ];
 
-      perSystem = { pkgs, system, ... }:
+      perSystem =
+        { pkgs, system, ... }:
         let
           pkgs = import nixpkgs {
             inherit system;
             overlays = [ foolnotion.overlay ];
           };
-          stdenv = pkgs.llvmPackages_latest.stdenv;
-          pythonPkgs = pkgs.python3Packages.override {
-            overrides = self: super: {
-              nanobind = super.nanobind.overridePythonAttrs (old: {
-                doCheck = false;
-                build-system = old.build-system ++ [ pythonPkgs.typing-extensions ];
-              });
-            };
-          };
-          nanobind = pythonPkgs.nanobind;
+          #stdenv = pkgs.llvmPackages_latest.stdenv;
+          inherit (pkgs.llvmPackages_latest) stdenv;
         in
         rec {
           devShells.default = stdenv.mkDerivation {
@@ -39,13 +43,20 @@
               clang-tools
               cppcheck
               gdb
+              eve
               doxygen
               valgrind
-              nanobind
+              pkgs.python3Packages.nanobind
             ];
-            buildInputs = packages.default.buildInputs
-              ++ (with pkgs; [ boost doctest gsl pkg-config ])
-             ++ (with pkgs; if pkgs.stdenv.isx86_64 then [ linasm ] else []);
+            buildInputs =
+              packages.default.buildInputs
+              ++ (with pkgs; [
+                boost
+                doctest
+                gsl
+                pkg-config
+              ])
+              ++ (with pkgs; if pkgs.stdenv.isx86_64 then [ linasm ] else [ ]);
           };
 
           packages.vstat = stdenv.mkDerivation {
@@ -55,9 +66,7 @@
             buildInputs = with pkgs; [ eve ];
 
             cmakeFlags = [
-              "-DCMAKE_CXX_FLAGS=${
-                if pkgs.stdenv.hostPlatform.isx86_64 then "-march=x86-64" else ""
-              }"
+              "-DCMAKE_CXX_FLAGS=${if pkgs.stdenv.hostPlatform.isx86_64 then "-march=x86-64" else ""}"
               "-DCPM_USE_LOCAL_PACKAGES=1"
             ];
           };
@@ -66,7 +75,7 @@
             name = "vstat";
             src = self;
             nativeBuildInputs = with pkgs; [ cmake ];
-            buildInputs = packages.vstat.buildInputs ++ [ nanobind ];
+            buildInputs = packages.vstat.buildInputs ++ [ pkgs.python3Packages.nanobind ];
             cmakeFlags = packages.vstat.cmakeFlags ++ [ "-Dvstat_BUILD_PYTHON=ON" ];
           };
 
