cd753d0467ccc96389fb969551eccceca97f38d1
Bogdan Burlacu <bogdan.burlacu@pm.me>
2026-02-12 11:23:03 +0200

    fix cmake install config

--- cmake/install-config.cmake
+++ cmake/install-config.cmake
@@ -1,1 +1,8 @@
+include(CMakeFindDependencyMacro)
+
+# INTERFACE dependencies
+find_dependency(eve CONFIG REQUIRED)
+
 include("${CMAKE_CURRENT_LIST_DIR}/vstatTargets.cmake")
+check_required_components(vstat)
+
